ef5bd6accaa0c9e668e5b7ecceb4d7a914a977ff
Than Than Moe <moelaybtu@gmail.com>
2023-11-10 16:41:08 +0630

    MVC [CRUD]

---
+++ TTMDotNetCore.MvcApp/AppDB/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TTMDotNetCore.MvcApp.Models;
+
+namespace TTMDotNetCore.MvcApp.AppDB
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+
+
+    }
+}
+
+
---
+++ TTMDotNetCore.MvcApp/Models/BlogListResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TTMDotNetCore.MvcApp.Models
+{
+    public class BlogListResponseModel
+    {
+        public List<BlogDataModel> BlogList { get; set; }
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+        public int PageCount { get; set; }
+        public int PageRowCount { get; set; }
+    }
+}
