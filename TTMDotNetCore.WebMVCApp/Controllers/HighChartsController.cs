d78f64a2fc55cc6a8cb7c1d9428006383a215036
Than Than Moe <thanthanmoe@acedatasystems.com>
2023-12-24 01:05:19 +0630

    [complete] High Charts

---
+++ TTMDotNetCore.WebMVCApp/Controllers/HighChartsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TTMDotNetCore.WebMVCApp.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TTMDotNetCore.WebMVCApp.Controllers
+{
+    public class HighChartsController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+        public IActionResult BasicColumnChart()
+        {
+            HighChartBasicColumnChartModel model = new HighChartBasicColumnChartModel
+            {
+                Categories = new List<string> { "USA", "China", "Brazil", "EU", "India", "Russia" },
+                Name = new List<string> { "Corn", "Wheat" },
+                Data = new List<List<int>>
+                {
+                    new List<int> { 406292, 260000, 107000, 68300, 27500, 14500 },
+                    new List<int> { 51086, 136000, 5500, 141000, 107180, 77000 }
+                }
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ TTMDotNetCore.WebMVCApp/Models/HighChartBasicColumnChartModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TTMDotNetCore.WebMVCApp.Models
+{
+    public class HighChartBasicColumnChartModel
+    {
+        public List<List<int>> Data { get; set; }
+        public List<string> Name { get; set; }
+        public List<string> Categories { get; set; }
+    }
+}
