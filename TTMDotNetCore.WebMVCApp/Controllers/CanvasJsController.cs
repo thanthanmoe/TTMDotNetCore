49200ba0fd834fa3aa76cb1769e1630330c1e02b
Than Than Moe <thanthanmoe@acedatasystems.com>
2023-12-24 01:04:00 +0630

    [complete] Canvas Charts

---
+++ TTMDotNetCore.WebMVCApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TTMDotNetCore.WebMVCApp.Models;
+
+namespace TTMDotNetCore.WebMVCApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+        public IActionResult PyramidChart()
+        {
+			CanvasChartPyramidChartModel model = new CanvasChartPyramidChartModel
+			{
+				Categories = new List<string> { "Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7" },
+				Data = new List<int> { 300, 50, 100, 300, 50, 100, 20 },
+
+			};
+			return View(model);
+        }
+    }
+}
