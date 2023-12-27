9e0929c40099f90dfc3b15bfd16ae7257c9c1266
Than Than Moe <thanthanmoe@acedatasystems.com>
2023-12-27 16:57:02 +0630

    [complete]Theme

--- TTMDotNetCore.ThemeMvcApp/Controllers/HomeController.cs
+++ TTMDotNetCore.ThemeMvcApp/Controllers/HomeController.cs
@@ -23,6 +23,7 @@ namespace TTMDotNetCore.ThemeMvcApp.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
