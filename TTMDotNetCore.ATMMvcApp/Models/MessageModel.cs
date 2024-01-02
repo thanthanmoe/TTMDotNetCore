07c3cdb861950e95f4e632ba8811fe8f78ec0e49
Than Than Moe <thanthanmoe@acedatasystems.com>
2024-01-02 15:33:52 +0630

    [edit]Model

---
+++ TTMDotNetCore.ATMMvcApp/Models/MessageModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTMDotNetCore.ATMMvcApp.Models
+{
+  
+    public class MessageModel
+    {
+        public MessageModel() { }
+        public MessageModel(bool isSuccess, string message)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+}
