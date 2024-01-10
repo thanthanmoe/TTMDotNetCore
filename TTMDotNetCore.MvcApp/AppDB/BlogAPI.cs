d13b8af4a8ba84d7e2786805de87fc75ef9e0644
Than Than Moe <moelaybtu@gmail.com>
2024-01-10 19:53:11 +0630

    [InProgress]Refit

---
+++ TTMDotNetCore.MvcApp/AppDB/BlogAPI.cs
@@ -0,0 +1,30 @@
+﻿using Refit;
+using System.Threading.Tasks;
+using TTMDotNetCore.MvcApp.Models;
+
+namespace TTMDotNetCore.MvcApp.AppDB
+{
+	public interface BlogAPI
+	{
+		[Get("/api/Blog")]
+		Task<BlogListResponseModel> GetBlogs();
+
+		[Get("/api/Blog/{pageNo}/{pageSize}")]
+		Task<BlogListResponseModel> GetBlog(int pageNo, int pageSize = 10);
+
+		[Get("/api/Blog/{id}")]
+		Task<BlogResponseModel> EditBlog(int id);
+
+		[Post("/api/Blog")]
+		Task<BlogResponseModel> CreateBlog(BlogDataModel blog);
+
+		[Put("/api/Blog/{id}")]
+		Task<BlogResponseModel> UpdateBlog(int id, BlogDataModel blog);
+
+		[Patch("/api/Blog/{id}")]
+		Task<BlogResponseModel> PathBlog(int id, BlogDataModel blog);
+
+		[Delete("/api/Blog/{id}")]
+		Task<BlogResponseModel> DeleteBlog(int id);
+	}
+}
---
+++ TTMDotNetCore.MvcApp/Controllers/BlogRefitController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TTMDotNetCore.MvcApp.AppDB;
+using TTMDotNetCore.MvcApp.Models;
+namespace TTMDotNetCore.MvcApp.Controllers
+{
+	public class BlogRefitController : Controller
+	{
+		private readonly BlogAPI _blogApi;
+
+		public BlogRefitController(BlogAPI blogApi)
+		{
+			_blogApi = blogApi;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var model = await _blogApi.GetBlogs();
+			TempData["ControllerName"] = "Blogrefit";
+			return View(model);
+		}
+
+		public IActionResult CreateForm()
+		{
+			TempData["ControllerName"] = "Blogrefit";
+			return View("Create");
+		}
+
+		public async Task<IActionResult> Create(BlogDataModel reqModel)
+		{
+			BlogResponseModel model = await _blogApi.CreateBlog(reqModel);
+			return Redirect("/Blogrefit");
+		}
+
+		public async Task<IActionResult> Edit(int id)
+		{
+			BlogResponseModel model = await _blogApi.EditBlog(id);
+			TempData["ControllerName"] = "Blogrefit";
+			return View(model);
+		}
+
+		public async Task<IActionResult> Update(int id, BlogDataModel reqModel)
+		{
+			BlogResponseModel model = await _blogApi.UpdateBlog(id, reqModel);
+
+			return Redirect("/Blogrefit");
+		}
+
+		public async Task<IActionResult> Delete(int id)
+		{
+			BlogResponseModel model = await _blogApi.DeleteBlog(id);
+
+			return Redirect("/Blogrefit");
+		}
+
+	}
+}
