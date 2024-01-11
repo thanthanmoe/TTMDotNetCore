ddf295c842db0efb0460337e4bfab55c46d02b1e
Than Than Moe <moelaybtu@gmail.com>
2024-01-11 23:05:34 +0630

    [complete] Mvc CRUD of Refit

--- TTMDotNetCore.MvcApp/Controllers/BlogRefitController.cs
+++ TTMDotNetCore.MvcApp/Controllers/BlogRefitController.cs
@@ -1,55 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using TTMDotNetCore.MvcApp.AppDB;
 using TTMDotNetCore.MvcApp.Models;
+using TTMDotNetCore.MvcApp.Interfaces;
+
 namespace TTMDotNetCore.MvcApp.Controllers
 {
 	public class BlogRefitController : Controller
 	{
-		private readonly BlogAPI _blogApi;
+		private readonly IBlogApi _blogAPI;
 
-		public BlogRefitController(BlogAPI blogApi)
+		public BlogRefitController(IBlogApi blogApi)
 		{
-			_blogApi = blogApi;
+			_blogAPI = blogApi;
 		}
 
 		public async Task<IActionResult> Index()
 		{
-			var model = await _blogApi.GetBlogs();
+			BlogResponseModels model= await _blogAPI.GetBlogs();
 			TempData["ControllerName"] = "BlogRefit";
 			return View(model);
 		}
 
-		public IActionResult CreateForm()
+		public IActionResult Create()
 		{
 			TempData["ControllerName"] = "BlogRefit";
 			return View("Create");
 		}
 
-		public async Task<IActionResult> Create(BlogDataModel reqModel)
+		public async Task<IActionResult> Save(BlogDataModel reqModel)
 		{
-			BlogResponseModel model = await _blogApi.CreateBlog(reqModel);
-			return Redirect("/BlogRefit");
+			BlogResponseModel model = await _blogAPI.CreateBlog(reqModel);
+			return Redirect("/BlogRefit/Index");
 		}
 
 		public async Task<IActionResult> Edit(int id)
 		{
-			BlogResponseModel model = await _blogApi.EditBlog(id);
+			BlogResponseModel model = await _blogAPI.EditBlog(id);
 			TempData["ControllerName"] = "BlogRefit";
 			return View(model);
 		}
 
 		public async Task<IActionResult> Update(int id, BlogDataModel reqModel)
 		{
-			BlogResponseModel model = await _blogApi.UpdateBlog(id, reqModel);
+			BlogResponseModel model = await _blogAPI.UpdateBlog(id, reqModel);
 
 			return Redirect("/BlogRefit");
 		}
 
 		public async Task<IActionResult> Delete(int id)
 		{
-			BlogResponseModel model = await _blogApi.DeleteBlog(id);
+			BlogResponseModel model = await _blogAPI.DeleteBlog(id);
 
-			return Redirect("/BlogRefit");
+			return Redirect("/BlogRefit/Index");
 		}
 
 	}
