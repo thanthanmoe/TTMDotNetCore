de30d1910e63a291604bf0249c10b5dedb1764c5
Than Than Moe <moelaybtu@gmail.com>
2024-01-24 20:43:11 +0630

    [complete] Chat App using SignalR

---
+++ TTMDotNetCore.SignalChatApp/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
+using TTMDotNetCore.SignalChatApp.Models;
+using TTMDotNetCore.SignalRChatApp.Hubs;
+
+namespace TTMDotNetCore.SignalChatApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHubContext<ChatHub> _chatHub;
+
+        public HomeController(ILogger<HomeController> logger, IHubContext<ChatHub> chatHub)
+        {
+            _logger = logger;
+            _chatHub = chatHub;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        private static int count = 0;
+        public async Task<IActionResult> Order()
+        {
+            count++;
+            await _chatHub.Clients.All.SendAsync("ClientOrderReceiveMessage", count);
+            return Redirect("/");
+        }
+
+        public IActionResult Chat()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TTMDotNetCore.SignalChatApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TTMDotNetCore.SignalChatApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
